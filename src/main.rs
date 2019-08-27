42790cfc39654ea24a46516dad9fc986fd130948
AppleTheGolden <scotsbox@protonmail.com>
2019-08-27 17:03:18 +0200

    Rewrite in Rust

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use reqwest::get;
+
+use serenity::{
+    model::{channel::Message,id::UserId, gateway::Ready},
+    prelude::*,
+    utils::Colour,
+};
+
+use log::{debug, error, info};
+
+mod parsers;
+use parsers::PARSERS;
+
+
+fn main() {
+    kankyo::load(false).expect("Expected a .env file");
+    env_logger::init();
+
+    let token = std::env::var("DISCORD_TOKEN").expect("Expected a token in $DISCORD_TOKEN");
+
+    let mut client = Client::new(&token, Handler).expect("Err creating client");
+
+    if let Err(why) = client.start() {
+        error!("Client error: {:?}", why);
+    }
+}
+
+fn common_mistakes(input: &str) -> Vec<(&str, String)> {
+    PARSERS.iter().flat_map(|m| m(input)).collect()
+}
+
+/// Takes a string of an URL, returns the content.
+/// Helper for Error Handling.
+fn get_log(link: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let link: reqwest::Url = link.parse()?;
+    Ok(get(link)?.text()?)
+}
+
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot {
+            return
+        }
+
+        if msg.is_private() {
+            if let Err(why) = msg.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title("<:backgroundcat:280120125284417536>A bot to parse logfiles on the MultiMC discord<:backgroundcat:280120125284417536>");
+                    let creator_name = match UserId::from(185461862878543872).to_user(&ctx) { //185461862878543872
+                        Ok(o) => o.tag(),
+                        Err(why) => {error!("Couldn't get info about creator: {}", why); "<Error getting name>".to_string()}
+                    };
+                    e.colour(Colour::DARK_TEAL);
+                    e.description(format!(r"
+                Developed by {}.
+                To start, just post a https://paste.ee link in the Discord.
+                
+                [Source Code available under AGPLv3](https://gitlab.com/Scotsguy/background-cat)
+                ", creator_name));
+                    e
+                });
+                m
+            }) {
+                error!("Couldn't send info message: {}", why)
+            }
+        }
+
+        lazy_static! {
+            static ref PASTEE_REGEX: Regex = Regex::new(r"https:/{2}paste.ee/p/[^\s/]+").unwrap();
+        }
+
+        if let Some(link) = PASTEE_REGEX.find(&msg.content) {
+            info!(
+                "Found paste.ee link: {} in message {}",
+                link.as_str(),
+                msg.id
+            );
+
+            let link = link.as_str().replacen("/p/", "/r/", 1);
+            let log = match get_log(&link) {
+                Ok(o) => o,
+                Err(_) => return,
+            };
+            debug!("Content of log: {}", log);
+
+            let mistakes = common_mistakes(&log);
+            debug!("Mistakes found: {:?}", mistakes);
+
+            if let Err(why) = msg.channel_id.send_message(&ctx.http, |m| {
+                m.embed(|e| {
+                    e.title("Automated Response: (Warning: Experimental)");
+                    e.colour(Colour::DARK_TEAL);
+                    for i in mistakes.iter() {
+                        e.field(i.0, &i.1, true);
+                    }
+                    e.footer(|f| {
+                        f.icon_url("https://cdn.discordapp.com/emojis/280120125284417536.png?v=1");
+                        f.text("This might not solve your problem, but it could be worth a try")
+                    });
+                    debug!("Embed: {:?}", e);
+                    e
+                });
+                debug!("Embed: {:?}", m);
+                m
+            }) {
+                error!("Couldn't send message: {}", why)
+            }
+        };
+    }
+
+    // TODO: delete on reaction
+
+    fn ready(&self, _: Context, ready: Ready) {
+        info!(
+            "{}#{} is connected!",
+            ready.user.name, ready.user.discriminator
+        );
+    }
+}
---
+++ src/parsers.rs
@@ -0,0 +1,85 @@
+#![deny(dead_code)]
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+pub type Check = fn(&str) -> Option<(&str, String)>;
+
+pub static PARSERS: [Check; 6] = [
+    multimc_in_program_files,
+    server_java,
+    java_version,
+    id_range_exceeded,
+    java_architecture,
+    ram_amount,
+];
+
+use log::warn;
+
+fn multimc_in_program_files(log: &str) -> Option<(&str, String)> {
+    const TRIGGER: &str = "Minecraft folder is:\nC:/Program Files";
+    if log.contains(TRIGGER) {
+        Some(("‼", "Your MultiMC installation is in Program Files, where MultiMC doesn't have permission to write.\nMove it somewhere else, like your Desktop.".to_string()))
+    } else {
+        None
+    }
+}
+
+fn server_java(log: &str) -> Option<(&str, String)> {
+    const TRIGGER: &str = "-Bit Server VM warning";
+    if log.contains(TRIGGER) {
+        Some(("‼", "You're using the server version of Java. You should install the desktop version from [this link](https://java.com/en/download/manual.jsp).".to_string()))
+    } else {
+        None
+    }
+}
+fn id_range_exceeded(log: &str) -> Option<(&str, String)> {
+    const TRIGGER: &str =
+        "java.lang.RuntimeException: Invalid id 4096 - maximum id range exceeded.";
+    if log.contains(TRIGGER) {
+        Some(("‼", "You've exceeded the hardcoded ID Limit. Remove some mods, or install [this one](https://www.curseforge.com/minecraft/mc-mods/notenoughids)".to_string()))
+    } else {
+        None
+    }
+}
+
+fn java_version(log: &str) -> Option<(&str, String)> {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r"Java is version (1.)??(?P<ver>6|7|9|10|11|12)+\..+,").unwrap();
+    }
+    if let Some(capture) = RE.captures(log) {
+        Some(("❗", format!("You're using Java {}. Versions other than Java 8 are not designed to be used with Minecraft and may cause issues. You should install Java 8 from [this link](https://java.com/en/download/manual.jsp).",
+            capture.name("ver")?.as_str())))
+    } else {
+        None
+    }
+}
+
+fn java_architecture(log: &str) -> Option<(&str, String)> {
+    const TRIGGER: &str = "Your Java architecture is not matching your system architecture.";
+    if log.contains(TRIGGER) {
+        Some(("❗", "You're using 32-bit Java. You should install 64-bit Java from [this link](https://java.com/en/download/manual.jsp).".to_string()))
+    } else {
+        None
+    }
+}
+
+fn ram_amount(log: &str) -> Option<(&str, String)> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"-Xmx(?P<amount>\d+)m[,\]]").unwrap();
+    }
+    if let Some(capture) = RE.captures(log) {
+        let amount = capture.name("amount")?.as_str().parse::<f32>();
+        let amount = match amount {
+            Ok(o) => o,
+            Err(why) => {warn!("Couldn't parse RAM amount: {:?}", why); return None}
+        };
+        let amount = amount / 1000.0; // Megabytes => Gigabytes
+
+        if amount > 10.0 {
+             return Some(("⚠", format!("You have allocated {}GB of RAM to Minecraft. [This is too much and can cause lagspikes.](https://vazkii.net/#blog/ram-explanation)", amount )))
+        };
+    }
+    None
+}
